f020231f6d59b85692d7cd2cd993e404e3393d9f
phamhieu <phamhieu1998@gmail.com>
2021-03-02 14:20:10 +0800

    initial commit

---
+++ src/components/BaseGrid.tsx
@@ -0,0 +1,8 @@
+//@ts-nocheck
+import * as React from 'react';
+import { RevoGrid } from '@revolist/revogrid-react';
+
+const BaseGrid = props => {
+  return <RevoGrid {...props} />;
+};
+export default BaseGrid;
