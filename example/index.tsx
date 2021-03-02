7af41ea4e37bdeac3f085697776c31a0f73dd709
phamhieu <phamhieu1998@gmail.com>
2021-03-02 15:15:40 +0800

    chore: setup glide-data-grid

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,58 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import SupabaseGrid from '../dist';
+import {
+  EditableGridCell,
+  GridCell,
+  GridCellKind,
+} from '@glideapps/glide-data-grid';
 
 const App = () => {
+  function getData([col, row]: readonly [number, number]): GridCell {
+    let n: number;
+    if (col === 0) {
+      n = row;
+    } else if (col === 1) {
+      n = row * row;
+    } else if (col === 2) {
+      const value = !(row % 2) ? false : true;
+      return {
+        kind: GridCellKind.Boolean,
+        data: value,
+        showUnchecked: true,
+        allowOverlay: true,
+        allowEdit: true,
+      };
+    } else {
+      throw new Error('This should not happen');
+    }
+    return {
+      kind: GridCellKind.Text,
+      data: n + '',
+      displayData: n.toString(),
+      allowOverlay: true,
+    };
+  }
+
+  function onCellEdited(
+    cell: readonly [number, number],
+    newValue: EditableGridCell
+  ) {
+    console.log('cell', cell, 'newValue', newValue);
+  }
+
   return (
     <div>
-      <Thing />
+      <SupabaseGrid
+        columns={[
+          { title: 'Number', width: 100 },
+          { title: 'Square', width: 100 },
+          { title: 'Is Cool', width: 100 },
+        ]}
+        getCellContent={getData}
+        onCellEdited={onCellEdited}
+      />
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,33 @@
 import * as React from 'react';
+import DataEditor, {
+  DataEditorContainer,
+  EditableGridCell,
+  GridCell,
+  GridColumn,
+} from '@glideapps/glide-data-grid';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
+type Props = {
+  columns: GridColumn[];
+  getCellContent: (cell: readonly [number, number]) => GridCell;
+  onCellEdited?: (
+    cell: readonly [number, number],
+    newValue: EditableGridCell
+  ) => void;
 };
+
+const SupabaseGrid = ({ columns, getCellContent, onCellEdited }: Props) => {
+  return (
+    <>
+      <DataEditorContainer width={500} height={300}>
+        <DataEditor
+          columns={columns}
+          rows={10}
+          getCellContent={getCellContent}
+          onCellEdited={onCellEdited}
+        />
+      </DataEditorContainer>
+      <div id="portal" />
+    </>
+  );
+};
+export default SupabaseGrid;
