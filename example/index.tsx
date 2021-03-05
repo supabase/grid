2abde926d10645f65d6c5b6f9bb01055338c7c04
phamhieu <phamhieu1998@gmail.com>
2021-03-06 07:51:37 +0800

    refactor: clean up example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import SupabaseGrid from '../.';
+import SupabaseGrid from '../dist';
 // import countries from './countries'
 
 const App = () => {
   return (
-    <div>
+    <div style={{ height: '20rem' }}>
       <SupabaseGrid
         table="countries"
         // table={countries}
@@ -15,7 +15,6 @@ const App = () => {
           supabaseKey:
             'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjE0ODE0ODc4LCJleHAiOjE5MzAzOTA4Nzh9.CjLbUVvuw0YrFcjpqDMUMZmef2_v3MFjCo4-Z4C9_0Q',
         }}
-        gridProps={{ width: 1000, height: 600 }}
       />
     </div>
   );
