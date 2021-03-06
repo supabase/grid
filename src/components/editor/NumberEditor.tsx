3d02a5406a6b51e31223c437f029be20eb48bcfd
phamhieu <phamhieu1998@gmail.com>
2021-03-06 10:22:39 +0800

    chore: use html.input component with NumberEditor

--- src/components/editor/NumberEditor.tsx
+++ src/components/editor/NumberEditor.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
-import { Input } from '@supabase/ui';
 import { EditorProps } from 'react-data-grid';
+
+function autoFocusAndSelect(input: HTMLInputElement | null) {
+  input?.focus();
+  input?.select();
+}
 
 export default function NumberEditor<TRow, TSummaryRow = unknown>({
   row,
@@ -8,17 +12,15 @@ export default function NumberEditor<TRow, TSummaryRow = unknown>({
   onRowChange,
   onClose,
 }: EditorProps<TRow, TSummaryRow>) {
-  function onChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    onRowChange({ ...row, [column.key]: event.target.value });
-  }
-
   return (
-    <Input
-      autofocus
+    <input
+      ref={autoFocusAndSelect}
       value={(row[column.key as keyof TRow] as unknown) as string}
-      type="number"
-      onChange={onChange}
+      onChange={event =>
+        onRowChange({ ...row, [column.key]: event.target.value })
+      }
       onBlur={() => onClose(true)}
+      type="number"
     />
   );
 }
