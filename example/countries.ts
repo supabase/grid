d7efd1b6db464750b3770a83ba96b615991b068c
phamhieu <phamhieu1998@gmail.com>
2021-03-22 12:26:55 +0800

    fix: example table definitions

--- example/countries.ts
+++ example/countries.ts
@@ -1,9 +1,6 @@
 import { SupaColumn, SupaTable } from '../dist';
 
 const colId: SupaColumn = {
-  tableId: 16523,
-  schema: 'public',
-  id: '16523.1',
   position: 1,
   name: 'id',
   defaultValue: null,
@@ -13,13 +10,9 @@ const colId: SupaColumn = {
   isGeneratable: true,
   isNullable: false,
   isUpdatable: true,
-  enums: [],
-  comment: null,
+  enum: [],
 };
 const colName: SupaColumn = {
-  tableId: 16523,
-  schema: 'public',
-  id: '16523.2',
   position: 2,
   name: 'name',
   defaultValue: null,
@@ -29,13 +22,10 @@ const colName: SupaColumn = {
   isGeneratable: false,
   isNullable: true,
   isUpdatable: true,
-  enums: [],
+  enum: [],
   comment: 'Full country name.',
 };
 const colIso2: SupaColumn = {
-  tableId: 16523,
-  schema: 'public',
-  id: '16523.3',
   position: 3,
   name: 'iso2',
   defaultValue: null,
@@ -45,13 +35,10 @@ const colIso2: SupaColumn = {
   isGeneratable: false,
   isNullable: false,
   isUpdatable: true,
-  enums: [],
+  enum: [],
   comment: 'ISO 3166-1 alpha-2 code.',
 };
 const colIso3: SupaColumn = {
-  tableId: 16523,
-  schema: 'public',
-  id: '16523.4',
   position: 4,
   name: 'iso3',
   defaultValue: null,
@@ -61,13 +48,10 @@ const colIso3: SupaColumn = {
   isGeneratable: false,
   isNullable: true,
   isUpdatable: true,
-  enums: [],
+  enum: [],
   comment: 'ISO 3166-1 alpha-3 code.',
 };
 const colLocalName: SupaColumn = {
-  tableId: 16523,
-  schema: 'public',
-  id: '16523.5',
   position: 5,
   name: 'local_name',
   defaultValue: null,
@@ -77,13 +61,10 @@ const colLocalName: SupaColumn = {
   isGeneratable: false,
   isNullable: true,
   isUpdatable: true,
-  enums: [],
+  enum: [],
   comment: 'Local variation of the name.',
 };
 const colContinent: SupaColumn = {
-  tableId: 16523,
-  schema: 'public',
-  id: '16523.6',
   position: 6,
   name: 'continent',
   defaultValue: null,
@@ -93,7 +74,7 @@ const colContinent: SupaColumn = {
   isGeneratable: false,
   isNullable: true,
   isUpdatable: true,
-  enums: [
+  enum: [
     'Africa',
     'Antarctica',
     'Asia',
@@ -102,15 +83,12 @@ const colContinent: SupaColumn = {
     'North America',
     'South America',
   ],
-  comment: null,
 };
 const countries: SupaTable = {
-  id: 16523,
   name: 'countries',
   comment: 'Full list of countries.',
   schema: 'public',
   totalRows: 249,
   columns: [colId, colName, colIso2, colIso3, colLocalName, colContinent],
-  relationships: [],
 };
 export default countries;
