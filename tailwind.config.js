c179d1befa56ce0ce3b6f8d1ace357748e721b76
phamhieu <phamhieu1998@gmail.com>
2021-04-12 09:10:10 +0800

    fix: update tailwindcss to v2.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
--- tsdx.config.js
+++ tsdx.config.js
@@ -3,7 +3,7 @@ const replace = require('@rollup/plugin-replace');
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-const tailwindcssjit = require('@tailwindcss/jit');
+const tailwindcss = require('tailwindcss');
 const tailwindconfig = require('./tailwind.config');
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
     config.plugins.push(
       postcss({
         plugins: [
-          tailwindcssjit(tailwindconfig),
+          tailwindcss(tailwindconfig),
           autoprefixer(),
           cssnano({
             preset: 'default',
