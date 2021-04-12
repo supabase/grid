6d53910637e79411a5e0204e19d85bbd5ca95b2a
phamhieu <phamhieu1998@gmail.com>
2021-04-12 09:39:00 +0800

    fix: example

--- example/index.tsx
+++ example/index.tsx
@@ -8,21 +8,39 @@ import { SupabaseGrid } from '../.';
 // import countries from './countries'
 
 const App = () => {
+  const [tableName, setName] = React.useState('countries_view');
+
+  function onClick() {
+    const name = tableName == 'countries_view' ? 'countries' : 'countries_view';
+    setName(name);
+  }
+
+  console.log('table name', tableName);
+
   // READONLY
   // using postgrest OpenApi description to retrieve table, column definition
   // so it supports both table, view
   return (
-    <div style={{ height: '100vh' }}>
-      <SupabaseGrid
-        table="countries_view"
-        storageRef="dqofwyqljsmbgrubmnzk"
-        clientProps={{
-          supabaseUrl: 'https://eswutwdkpzgacrlriize.supabase.net',
-          supabaseKey:
-            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNzA3NTU4OCwiZXhwIjoxOTMyNjUxNTg4fQ.h1TuIObta3ZBAgJ-XefdQGgFaFCFIEwQIlSHxcd4o18',
-        }}
-        gridProps={{ height: '100%' }}
-      />
+    <div>
+      <div style={{ height: '5vh' }}>
+        <button onClick={onClick}>Change View</button>
+      </div>
+      <div style={{ height: '95vh' }}>
+        <SupabaseGrid
+          table={tableName}
+          editable={tableName == 'countries'}
+          storageRef="dqofwyqljsmbgrubmnzk"
+          clientProps={{
+            supabaseUrl: 'https://eswutwdkpzgacrlriize.supabase.net',
+            supabaseKey:
+              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNzA3NTU4OCwiZXhwIjoxOTMyNjUxNTg4fQ.h1TuIObta3ZBAgJ-XefdQGgFaFCFIEwQIlSHxcd4o18',
+          }}
+          gridProps={{ height: '100%' }}
+          onDeleteColumn={columnName => {
+            console.log('delete column: ', columnName);
+          }}
+        />
+      </div>
     </div>
   );
 
