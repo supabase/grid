f7b1fc4ed370dc06fe866a83258abfe370fce444
phamhieu <phamhieu1998@gmail.com>
2021-05-17 14:49:17 +0700

    feat: create ModalPortal to show modal from document.body

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 export * from './BlockKeys';
 export * from './DropdownControl';
+export * from './ModalPortal';
 export * from './MonacoEditor';
 export * from './NullValue';
 export * from './SegmentedControl';
--- src/components/editor/ForeignKeyEditor.tsx
+++ src/components/editor/ForeignKeyEditor.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { EditorProps } from '@supabase/react-data-grid';
-import { Button, IconExternalLink } from '@supabase/ui';
-import { NullValue } from '../common';
+import { Button, Modal, Typography, IconExternalLink } from '@supabase/ui';
+import { ModalPortal, NullValue } from '../common';
 
 export function ForeignKeyEditor<TRow, TSummaryRow = unknown>({
   row,
@@ -9,14 +9,6 @@ export function ForeignKeyEditor<TRow, TSummaryRow = unknown>({
 }: EditorProps<TRow, TSummaryRow>) {
   const rawValue = row[column.key as keyof TRow] as unknown;
   const value = rawValue ? rawValue + '' : undefined;
-
-  function onExternalLinkClick(
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) {
-    event.stopPropagation();
-    // show foreign table modal, user can select the row to link
-    alert('Show foreign table modal');
-  }
 
   return (
     <div
@@ -27,12 +19,36 @@ export function ForeignKeyEditor<TRow, TSummaryRow = unknown>({
       <p className="m-0 flex-grow text-sm overflow-ellipsis">
         {value ? value : <NullValue />}
       </p>
-      <Button
-        type="text"
-        onClick={onExternalLinkClick}
-        icon={<IconExternalLink />}
-        style={{ padding: '3px' }}
-      />
+      <ForeignTableModal />
     </div>
   );
 }
+
+type ForeignTableModalProps = {};
+
+export const ForeignTableModal: React.FC<ForeignTableModalProps> = ({}) => {
+  const [visible, setVisible] = React.useState(false);
+
+  function toggle(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    if (event && event.stopPropagation) event.stopPropagation();
+    setVisible(!visible);
+  }
+
+  return (
+    <>
+      <Button
+        type="text"
+        onClick={toggle}
+        icon={<IconExternalLink />}
+        style={{ padding: '3px' }}
+      />
+      {visible && (
+        <ModalPortal>
+          <Modal visible={visible} onCancel={toggle} onConfirm={toggle}>
+            <Typography.Text>This is the content of the Modal</Typography.Text>
+          </Modal>
+        </ModalPortal>
+      )}
+    </>
+  );
+};
