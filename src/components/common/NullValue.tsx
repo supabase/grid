e984b25ca61da46d8edf7362c08b6e869fcdb6bc
Ankit Jena <ankitjena13@gmail.com>
2021-06-11 12:52:57 +0530

    fix: change formatting for null/empty text column (#50)

--- src/components/common/NullValue.tsx
+++ src/components/common/NullValue.tsx
@@ -3,5 +3,5 @@ import * as React from 'react';
 type NullValueProps = {};
 
 export const NullValue: React.FC<NullValueProps> = ({}) => {
-  return <span className="block">[null]</span>;
+  return <span className="block">NULL</span>;
 };
