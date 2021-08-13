a1c83d06d7b512f2a03251616b3e84de9ce8d97c
phamhieu <phamhieu1998@gmail.com>
2021-08-13 15:03:59 +0700

    feat: show RowHeader when there's selected rows

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import { useDispatch, useTrackedState } from '../../store';
-import { Button, Divider, IconPlus, IconTrash, Modal } from '@supabase/ui';
+import {
+  Button,
+  Divider,
+  IconDownload,
+  IconPlus,
+  IconTrash,
+  Modal,
+  Typography,
+} from '@supabase/ui';
 import FilterDropdown from './filter';
 import SortDropdown from './sort';
 import StatusLabel from './StatusLabel';
@@ -18,27 +26,34 @@ const Header: React.FC<HeaderProps> = ({
   headerActions,
 }) => {
   const state = useTrackedState();
-  const dispatch = useDispatch();
-  const { selectedRows, rows: allRows, editable } = state;
+  const { selectedRows } = state;
 
-  const [visible, setVisible] = React.useState(false);
+  return (
+    <div className="sb-grid-header">
+      <div className="sb-grid-header__inner">
+        {selectedRows.size > 0 ? (
+          <RowHeader />
+        ) : (
+          <DefaultHeader onAddColumn={onAddColumn} onAddRow={onAddRow} />
+        )}
+      </div>
+      <div className="sb-grid-header__inner">
+        {headerActions}
+        <StatusLabel />
+      </div>
+    </div>
+  );
+};
+export default Header;
 
-
-  const onRowsDelete = () => {
-    const rowIdxs = Array.from(selectedRows) as number[];
-    const rows = allRows.filter(x => rowIdxs.includes(x.idx));
-    const { error } = state.rowService!.delete(rows);
-    if (error) {
-      if (state.onError) state.onError(error);
-    } else {
-      dispatch({ type: 'REMOVE_ROWS', payload: { rowIdxs } });
-      dispatch({
-        type: 'SELECTED_ROWS_CHANGE',
-        payload: { selectedRows: new Set<React.Key>() },
-      });
-    }
-  };
-
+type DefaultHeaderProps = {
+  onAddColumn?: () => void;
+  onAddRow?: () => void;
+};
+const DefaultHeader: React.FC<DefaultHeaderProps> = ({
+  onAddColumn,
+  onAddRow,
+}) => {
   const renderNewColumn = (onAddColumn?: () => void) => {
     if (!onAddColumn) return null;
     return (
@@ -62,39 +77,92 @@ const Header: React.FC<HeaderProps> = ({
     );
   };
 
-  const renderDeleteRow = (selectedRowsLength: number, renderDeleteButton: boolean | undefined) => {
+  return (
+    <>
+      <RefreshButton />
+      <FilterDropdown />
+      <SortDropdown />
+      <Divider type="vertical" className="sb-grid-header__inner__divider" />
+      {renderNewColumn(onAddColumn)}
+      {renderAddRow(onAddRow)}
+    </>
+  );
+};
+
+type RowHeaderProps = {};
+const RowHeader: React.FC<RowHeaderProps> = ({}) => {
+  const state = useTrackedState();
+  const dispatch = useDispatch();
+  const [visible, setVisible] = React.useState(false);
+
+  const { selectedRows, rows: allRows, editable } = state;
+
+  const onRowsDelete = () => {
+    const rowIdxs = Array.from(selectedRows) as number[];
+    const rows = allRows.filter(x => rowIdxs.includes(x.idx));
+    const { error } = state.rowService!.delete(rows);
+    if (error) {
+      if (state.onError) state.onError(error);
+    } else {
+      dispatch({ type: 'REMOVE_ROWS', payload: { rowIdxs } });
+      dispatch({
+        type: 'SELECTED_ROWS_CHANGE',
+        payload: { selectedRows: new Set<React.Key>() },
+      });
+    }
+  };
+
+  const renderDeleteRow = (
+    selectedRowsLength: number,
+    renderDeleteButton: boolean | undefined
+  ) => {
     if (!renderDeleteButton) return null;
-    if(selectedRowsLength < 1) return null;
+    if (selectedRowsLength < 1) return null;
     return (
-      <Button 
+      <Button
         danger={true}
         style={{ padding: '4px 8px' }}
         icon={<IconTrash size="tiny" />}
         onClick={() => setVisible(true)}
       >
-        Delete {selectedRowsLength > 1 ? `${selectedRowsLength} rows`: `${selectedRowsLength} row`}
+        Delete{' '}
+        {selectedRowsLength > 1
+          ? `${selectedRowsLength} rows`
+          : `${selectedRowsLength} row`}
       </Button>
     );
-  }
+  };
 
   return (
-    <div className="sb-grid-header">
-      <div className="sb-grid-header__inner">
-        <RefreshButton />
-        <FilterDropdown />
-        <SortDropdown />
-        <Divider type="vertical" className="sb-grid-header__inner__divider" />
-        {renderNewColumn(onAddColumn)}
-        {renderAddRow(onAddRow)}
+    <>
+      <Typography.Text small>
+        {selectedRows.size > 1
+          ? `${selectedRows.size} rows selected`
+          : `${selectedRows.size} row selected`}
         {renderDeleteRow(selectedRows.size, editable)}
-      </div>
-      <div className="sb-grid-header__inner">
-        {headerActions}
-        <StatusLabel />
-      </div>
+      </Typography.Text>
+      <Button
+        type="text"
+        style={{ padding: '4px 8px' }}
+        icon={<IconDownload size="tiny" />}
+        onClick={() => {}}
+      >
+        Export to csv
+      </Button>
+      <Button
+        type="text"
+        danger={true}
+        style={{ padding: '4px 8px' }}
+        icon={<IconTrash size="tiny" />}
+        onClick={() => setVisible(true)}
+      >
+        Delete
+      </Button>
       <Modal
         closable
-        title={`Are you sure you want to delete ${selectedRows.size > 1 ? 'these rows': 'this row'}?`}
+        title={`Are you sure you want to delete ${
+          selectedRows.size > 1 ? 'these rows' : 'this row'
+        }?`}
         visible={visible}
         onCancel={() => setVisible(false)}
         onConfirm={() => {
@@ -102,7 +170,6 @@ const Header: React.FC<HeaderProps> = ({
           onRowsDelete();
         }}
       />
-    </div>
+    </>
   );
 };
-export default Header;
