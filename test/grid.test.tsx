99937092422618492927fcf81959eb0de1b75572
phamhieu <phamhieu1998@gmail.com>
2021-09-07 12:11:35 +0700

    chore: disable unit test

---
+++ test/grid.test.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { SupabaseGrid } from '../src';
+
+describe('<SupabaseGrid />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <SupabaseGrid
+        table=""
+        clientProps={{
+          supabaseUrl: '',
+          supabaseKey: '',
+        }}
+      />,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
