0314e824d6ab04865cdbc10560417071f79af5b3
phamhieu <phamhieu1998@gmail.com>
2021-09-08 07:51:40 +0700

    fix: jest unit test

---
+++ jest-setup.ts
@@ -0,0 +1,2 @@
+// jest-setup.ts
+import '@testing-library/jest-dom';
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// jest.config.js
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+};
