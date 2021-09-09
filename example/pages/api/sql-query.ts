9644209a61bc89c6bd356dfb48839fd7bd24ecc1
phamhieu <phamhieu1998@gmail.com>
2021-09-09 16:33:11 +0700

    fix: clean pg pool

--- example/pages/api/sql-query.ts
+++ example/pages/api/sql-query.ts
@@ -2,16 +2,22 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const { Pool } = require('pg');
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  let pool;
   try {
     if (!req.body?.query) throw { message: 'invalid query' };
 
     const { query } = req.body;
-    const pool = new Pool({
+    pool = new Pool({
       connectionString: process.env.NEXT_PUBLIC_CONNECTION_STRING,
     });
     const { rows } = await pool.query(query);
     res.status(200).json({ data: rows });
   } catch (error) {
     res.status(500).json({ error });
+  } finally {
+    // clean up pg pool
+    if (pool) {
+      pool.end().then(() => console.log('pool has ended'));
+    }
   }
 };
