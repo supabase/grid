8e155cee2193a0d6646f8365a55a3f7620614b6c
phamhieu <phamhieu1998@gmail.com>
2021-09-12 06:55:20 +0700

    fix: filter description

--- src/components/header/filter/Filter.constants.ts
+++ src/components/header/filter/Filter.constants.ts
@@ -5,8 +5,8 @@ export const FilterOperatorOptions = [
   { value: '<', label: '[ < ] less than' },
   { value: '>=', label: '[>=] greater than or equal' },
   { value: '<=', label: '[<=] less than or equal' },
-  { value: '~~', label: '[~~] LIKE operator' },
-  { value: '~~*', label: '[~~*] ILIKE operator' },
-  { value: 'in', label: '[in] one of a list of values' },
-  { value: 'is', label: '[is] checking for (null,not null,true,false)' },
+  { value: '~~', label: '[~~] like operator' },
+  { value: '~~*', label: '[~~*] ilike operator' },
+  { value: 'in', label: '[ in ] one of a list of values' },
+  { value: 'is', label: '[ is ] checking for (null,not null,true,false)' },
 ];
