7329da5d2df30d29cd8a8f69c74d2cbbe1f069cd
phamhieu <phamhieu1998@gmail.com>
2022-03-28 14:49:35 +0700

    chore: update example to tailwind v3

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -2,15 +2,12 @@ const ui = require('@supabase/ui/dist/config/ui.config.js');
 
 module.exports = ui({
   darkMode: 'class',
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@supabase/ui/dist/config/default-theme.js',
-    './../src/components/**/*.{js,ts,jsx,tsx}',
-    './../src/components/**/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 });
